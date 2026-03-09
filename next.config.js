359a8a2c57990f190f6804563795989d61207953
GloomyMantis <gloomymantis@gmail.com>
2026-03-09 22:35:05 +0200

    Add files via upload

--- next.config.js
+++ next.config.js
@@ -3,5 +3,8 @@ const nextConfig = {
   output: 'export',
   trailingSlash: true,
   images: { unoptimized: true },
+  experimental: {
+    missingSuspenseWithCSRBailout: false,
+  },
 }
 module.exports = nextConfig
